0233b7ade6d63dae49c1a0049955632be6879199
Bram Mittendorff <bram.mittendorff@dealerdirect.nl>
2019-10-02 10:30:18 +0200

    Added moar nodes

--- parallel_run.py
+++ parallel_run.py
@@ -2,9 +2,9 @@ from subprocess import Popen
 
 processes = []
 
-for counter in range(20):
+for counter in range(30):
     chrome_cmd = 'python3 start.py'
     processes.append(Popen(chrome_cmd, shell=True))
 
-for counter in range(20):
+for counter in range(30):
     processes[counter].wait()
